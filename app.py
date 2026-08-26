1fcbab61e1150901393bbdc1e0792c9e12f7e025
ujwalasanga06-commits <ujwalasanga06@gmail.com>
2026-08-26 22:39:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ except Exception as e:
 google_api_key = "AIzaSyC3g0qNbrOysUSD3lwSwOQnR6JUFZ9oAOI"  # మీ Gemini API Key ఇక్కడ ఇవ్వండి
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-latest",
+    model="gemini-1.5-pro",
     google_api_key=google_api_key,
     temperature=0
 )

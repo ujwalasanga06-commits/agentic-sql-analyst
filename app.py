304ce82388fcb30ca4ecb6424512abe47666f976
ujwalasanga06-commits <ujwalasanga06@gmail.com>
2026-08-27 11:28:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ except Exception as e:
     st.stop()
 
 # 2. Gemini API Setup
-google_api_key = "AIzaSyC3g0qNbrOysUSD3lwSwOQnR6JUFZ9oAOI"
+google_api_key = "AQ.Ab8RN6J4qn3G01DFbW6IJF7VTGcyTrqGG-mQRtJw_UP-VQQfYQ"
 genai.configure(api_key=google_api_key)
 model = genai.GenerativeModel('gemini-3.7-flash')
 
